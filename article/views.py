1248eda7c5cbcb4d4a14f2a5b34ecadd979782f0
janily <nanhuchenjie@163.com>
2015-04-18 00:13:27 +0800

    django tutorial

---
+++ article/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+
+# Create your views here.
+def home(request):
+	return HttpResponse("hello world")
+
+#def details(request,my_args):
+#	return HttpResponse("you're looking at my_args%s." % my_args)
+
+# def details(request,my_args):
+# 	post = Article.objects.all()[int(my_args)]
+# 	str = ("title=%s, cateory= %s ,date_time = %s, content = %s" % (post.title,post.cateory,post.date_time,post.content))
+# 	return HttpResponse(str)
+
+def home(request):
+	post_list = Article.objects.all()
+	return render(request,'home.html',{'post_list':post_list})
